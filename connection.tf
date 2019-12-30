2ba7a3e5d91243c34624969ff569ff35e16214a3
LuisDio <luis.diolindo@gmail.com>
2019-12-31 00:46:05 +0300

    initial commit

--- connection.tf
+++ connection.tf
@@ -1,1 +1,10 @@
-will server as init file
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "azurerm" {
+  subscription_id = "0"
+  client_id = "1"
+  client_secret = "2"
+  tenant_id = "3"
+}
